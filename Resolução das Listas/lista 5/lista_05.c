337f601c0a12e7e019aad2be71834c4ac8840ea4
Karolyne Rocha <karolrocha221@gmail.com>
2021-04-04 10:02:56 -0300

    Update lista_05.c

---
+++ Resolução das Listas/lista 5/lista_05.c
@@ -0,0 +1,19 @@
+// questão 01: A importância da análise de complexidade está na possibilidade de classificar a eficiência do código em questão e ela pode ser medida com a contagem dos comandos utilizados pelo algorítmo em questão. 
+
+
+// questão 02: 
+
+// a) Diferente da inserção no fim da lista, a inserção do inicio não depende de N. Portanto, sua complexidade não é relevante. 
+
+
+// b) considerando o pior caso, em que o elemento procurado está no fim da lista, a complexidade será igual ao número de comparações, que por sua vez, será igual ao número N de elementos na lista. A complexidade é O(n). 
+
+
+// c) O(log2N) 
+
+
+// questão 03: 
+
+// a) comparando as entradas de N e saidas de soma (que é a quantidade de vezes que o comando for, dominante do algoritmo, é executado) chegou-se a conclusão de que a complexidade seria 2n. Como o 2 não é quase nada comparado ao n, considera-se a complexidade do algartimo O(n). 
+
+// b) O for mais adentro do código vai rodar (n-1) vezes, enquanto que o primeiro for vai rodar, aproximadante n/2 vezes. Dessa forma, a complexidade fica em O(n^2). 
